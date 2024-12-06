29f7bdd13b7970ae42ae0c7e854c1138c361b62b
kiran187 <kirantupe1112@gmail.com>
2024-12-06 09:20:05 +0530

    Update conection.js

--- routes/conection.js
+++ routes/conection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var util = require("util");
 
 var conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database:"project03_nodejs"
+    host: "bdq7ljmg1sa8atf51kpi-mysql.services.clever-cloud.com",
+    user: "ue4tkz6bg2rqygtw",
+    password: "ue4tkz6bg2rqygtw",
+    database:"bdq7ljmg1sa8atf51kpi"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
